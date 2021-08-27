3cd5cfe1112b1f4dcc0d5da9274e5af23ae6c6a8
Adryan Rosa <adryanrosa374@gmail.com>
2021-08-26 22:42:23 -0300

    Adiciona validação email e mensagem de erro

--- src/sections/Contact/index.jsx
+++ src/sections/Contact/index.jsx
@@ -1,19 +1,42 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 import styles from './index.module.scss';
 import Button from '../../UI/Button';
+import { ReactComponent as ErrorIcon } from '../../images/icon-error.svg';
 
 function Contact() {
+  const [isValid, setIsValid] = React.useState(true);
+  const [email, setEmail] = React.useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (EMAIL_REGEX.test(email)) {
+      setEmail('');
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  };
+
   return (
     <section className={ styles.contact }>
       <p>35,000+ already joined</p>
       <h2>Stay up-to-date with what we’re doing</h2>
 
-      <form>
-        <input
-          type="text"
-          placeholder="Enter your email address"
-        />
+      <form onSubmit={ (event) => handleSubmit(event) }>
+        <div className={ isValid ? '' : styles.error }>
+          <input
+            value={ email }
+            onChange={ ({ target }) => setEmail(target.value) }
+            type="text"
+            placeholder="Enter your email address"
+          />
+          <ErrorIcon />
+          <span>Whoops, make sure its an email</span>
+        </div>
         <Button
           type="submit"
           color="red"
